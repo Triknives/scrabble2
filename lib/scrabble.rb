b2022d1c6c9dd2bcc61e4576c1d2c3675814533e
Student <example@example.com>
2019-12-10 11:21:03 -0800

    add two ways to solve the problem

--- lib/scrabble.rb
+++ lib/scrabble.rb
@@ -9,34 +9,44 @@ class Word
     @eight_point = {"J" => 8, "X" => 8}
     @ten_point = {"Q" => 10, "Z" => 10}
   end
-  # point_hashes = [@one_point, @two_point, ]
-  # point_hashes.each do |obj|
-  #   if obj.keys.include?(l)
-  #     counter += obj.fetch(l);
-  #   end
-  # end
+
 
   def scrabble_game()
     new_word = @word.upcase().split("")
     counter = 0
     new_word.each do | l |
-      if l == "Q" || l == "Z"
-        counter += @ten_point.fetch("Q")
-      elsif l == "J" || l == "X"
-        counter += @eight_point.fetch("J")
-      elsif l == "K"
-        counter += @five_point.fetch("K")
-      elsif l == "F" || l == "H" || l == "V" || l == "W" || l == "Y"
-        counter += @four_point.fetch("F")
-      elsif l == "B" || l == "C" || l == "M" || l == "P"
-        counter += @three_point.fetch("B")
-      elsif l == "G" || l == "D"
-        counter += @two_point.fetch("D")
-      elsif l == "A" || l == "E" || l == "I" || l == "O" || l == "U" || l == "L" || l == "N" || l == "R" || l == "S" || l == "T"
-        counter += @one_point.fetch("A")
+      point_hashes = [@one_point, @two_point, @three_point, @four_point, @five_point, @eight_point, @ten_point]
+      point_hashes.each do |obj|
+        if obj.keys.include?(l)
+          counter += obj.fetch(l);
+        end
       end
     end
-
     counter
   end
+
+  # def scrabble_game
+  #   new_word = @word.upcase().split("")
+  #   counter = 0
+  #   new_word.each do | l |
+  #     if l == "Q" || l == "Z"
+  #       counter += @ten_point.fetch("Q")
+  #     elsif l == "J" || l == "X"
+  #       counter += @eight_point.fetch("J")
+  #     elsif l == "K"
+  #       counter += @five_point.fetch("K")
+  #     elsif l == "F" || l == "H" || l == "V" || l == "W" || l == "Y"
+  #       counter += @four_point.fetch("F")
+  #     elsif l == "B" || l == "C" || l == "M" || l == "P"
+  #       counter += @three_point.fetch("B")
+  #     elsif l == "G" || l == "D"
+  #       counter += @two_point.fetch("D")
+  #     elsif l == "A" || l == "E" || l == "I" || l == "O" || l == "U" || l == "L" || l == "N" || l == "R" || l == "S" || l == "T"
+  #       counter += @one_point.fetch("A")
+  #     end
+  #   end
+  #   counter
+  # end
+
+
 end
